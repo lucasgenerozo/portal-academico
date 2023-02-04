07895ffe5dc35c66031617349b8b47c981ba082a
Lucas Generozo <luscanunes@hotmail.com>
2023-02-04 12:54:47 -0300

    Add a 'clickable' style in buttons, Add new icons/options, Refactored images names

--- _src/script.js
+++ _src/script.js
@@ -1,25 +1,41 @@
 const div_options = document.getElementById("options")
 
 const itemsImagesNames = [
-    'icon-lampada',
-    'icon-windows',
-    'icon-atestado',
-    'icon-teste'
+    'eventos-acad',
+    'microsoft',
+    'atestados',
+    'atividades-comp',
+    'autorizar',
+    'avaliacao-docente',
+    'biblioteca',
+    'bibliotecas-dig',
+    'boleto',
+    'campanhas-soli',
+    'certificados',
+    'impressao'
 ]
 
 const itemsText = [
     'Eventos Acadêmicos',
     'Aplicativos Misocroft',
     'Atestados',
-    'Atividades Complementares'
+    'Atividades Complementares',
+    'Autorizar Pessoas',
+    'Avaliação Docente',
+    'Biblioteca',
+    'Bibliotecas digitais',
+    'Boleto 2ª via',
+    'Campanhas Solidárias',
+    'Certificados',
+    'Consulta de Impressão e Xerox'
 ]
 
 function generateItems () {
     for (let i = 0; i < itemsImagesNames.length; i++) {
         div_options.innerHTML +=
         `
-        <div class="item" id="${itemsImagesNames[i]}">
-                <img src="_assets/${itemsImagesNames[i]}.png" alt="Icone de ${itemsText[i]}">
+        <div class="item" id="opt-${itemsImagesNames[i]}">
+                <img src="_assets/icon-${itemsImagesNames[i]}.png" alt="Icone de ${itemsText[i]}">
                 <h3>${itemsText[i]}</h3>
         </div>
         `
