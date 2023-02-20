39bee9333d89bf1aea3e10dacd173ff794a0a0bb
Lucas Generozo <luscanunes@hotmail.com>
2023-02-20 18:02:27 -0300

    Created Help alert, Responsivity and font-clamp of items maked

--- _src/login.js
+++ _src/login.js
@@ -2,6 +2,8 @@ const btnVisib = document.getElementById("icon-olho");
 const campoSenha = document.getElementById("user-password");
 const btnEntrar = document.getElementById("main");
 
+const alerta = document.getElementById('alert-help');
+
 btnEntrar.addEventListener('click', (e) => {
     e.preventDefault();
     // essa função é provisória enqnt nao é feita a de logar
@@ -16,3 +18,9 @@ function visibSenha() {
     campoSenha.classList = infos[1];
     campoSenha.type = infos[2];
 }
+
+function help(quo){
+    alerta.style.display = quo 
+}
+
+help('none')
